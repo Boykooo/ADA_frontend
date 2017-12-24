0f3048e604197fa938dceb7313b0af04a511b218
boytsov.andrew <boytsov.andrew@gmail.com>
2017-12-24 20:21:19 +0300

    Added Routing component, redirect from start to the welcome page

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from '../app.component';
+import { AppRoutingModule } from './routing/app-routing-module';
+import { WelcomeComponent } from '../components/welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/modules/routing/app-routing-module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WelcomeComponent} from '../../components/welcome/welcome.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomeComponent}
+];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule {
+}
